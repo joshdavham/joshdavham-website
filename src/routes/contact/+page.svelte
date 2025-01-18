38757a85f3f783d5000492861f69748440195860
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2025-01-18 12:59:54 -0800

    remove old files

--- src/routes/contact/+page.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-    <title>Contact</title>
-</svelte:head>
-
-<a href="mailto:hamiltonjoshuadavid@gmail.com" target="_blank" id="email-contact" class="underline text-blue-500 hover:text-blue-700">Email</a>
-<a href="https://github.com/joshdavham" target="_blank" id="github-contact" class="underline text-blue-500 hover:text-blue-700">GitHub</a>
-<a href="https://discord.com/users/joshdavham" target="_blank" id="discord-contact" class="underline text-blue-500 hover:text-blue-700">Discord</a>
